0f9612361fa9dc620628698c534d63bed5706bf1
zhaolandelong <zhaolandelong@58ganji.com>
2018-01-18 14:02:44 +0800

    sv

--- controllers/wechat.js
+++ controllers/wechat.js
@@ -40,44 +40,45 @@ module.exports = {
         token: 'zldl',
         appid: 'wxc04ca1666d781d0a',
         encodingAESKey: ''
-      }).middleware(async (message, ctx) => {
+    }).middleware(async (message, ctx) => {
         // TODO
-        console.log(message,ctx)
-      }),
+        console.log(message, ctx)
+    }),
     'POST /wechat': wechat({
         token: 'zldl',
         appid: 'wxc04ca1666d781d0a',
         encodingAESKey: ''
-      }).middleware(async (message, ctx) => {
+    }).middleware(async (message, ctx) => {
         let { MsgType, Content } = message;
-    if (MsgType === 'text') {
-        let reply;
-        switch (Content) {
-            case '12345':
-                reply = '上山打老虎';
-                break;
-            case 'kiki':
-                reply = '是我媳妇';
-                break;
-            default:
-                const msgs = [
-                    '我媳妇老漂亮了',
-                    '我媳妇会做饭',
-                    '我媳妇会煎药',
-                    '我媳妇吃的可多了',
-                    '我媳妇可能睡了',
-                    '我媳妇叫kiki',
-                    '我媳妇会打太极拳',
-                    '我媳妇总掉头发',
-                    '我媳妇可爱哭了',
-                    '我媳妇有点二'
-                ];
-                let rand = Math.round(Math.random() * msgs.length);
-                reply = msgs[rand];
+        console.log(Content);
+        if (MsgType === 'text') {
+            let reply;
+            switch (Content) {
+                case '12345':
+                    reply = '上山打老虎';
+                    break;
+                case 'kiki':
+                    reply = '是我媳妇';
+                    break;
+                default:
+                    const msgs = [
+                        '我媳妇老漂亮了',
+                        '我媳妇会做饭',
+                        '我媳妇会煎药',
+                        '我媳妇吃的可多了',
+                        '我媳妇可能睡了',
+                        '我媳妇叫kiki',
+                        '我媳妇会打太极拳',
+                        '我媳妇总掉头发',
+                        '我媳妇可爱哭了',
+                        '我媳妇有点二'
+                    ];
+                    let rand = Math.round(Math.random() * msgs.length);
+                    reply = msgs[rand];
+            }
+            return reply;
+        } else {
+            return '欢迎光临';
         }
-        return reply;
-    } else {
-        return '欢迎光临';
-    }
-      })
+    })
 };
